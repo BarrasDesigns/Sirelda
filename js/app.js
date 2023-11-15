78d77a5c3d9b16c0ce9ff8e42db3e7e2b556f43f
jobarv <jbarrera@motiontech.com.mx>
2023-11-15 16:52:17 -0600

    cambios solicitados 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+// REPRODUCE VIDEO 
+function getFullscreenElement() {
+  return document.fullscreenElement
+    || document.webkitFullscreenElement
+    || document.mozFullscreenElement
+    || document.msFullscreenElement
+}
+
+function iniciar() {
+  let boton = document.getElementById('btnIntro');
+  boton.addEventListener('click', presionar, false);
+}
+function presionar() {
+  let video = document.getElementById('intro');
+  video.play();
+}
+window.addEventListener('load', iniciar, false);
+
+function toggleFullscreen() {
+  if (getFullscreenElement()) {
+    document.exitFullscreen();
+  } else {
+    document.getElementById("intro").requestFullscreen().catch(console.log);
+    document.getElementById("intro").requestFullscreen().catch((e) => {
+      document.getElementById("intro").style.display = "block";
+      HTMLMediaElement.play();
+    });
+  }
+}
+
+function addEvent(element, evnt, funct) {
+  if (element.attachEvent)
+    return element.attachEvent('on' + evnt, funct);
+  else
+    return element.addEventListener(evnt, funct, false);
+}
+
+addEvent(
+  document.getElementById('fullscreen'),
+  'click',
+  function () { toggleFullscreen(); }
+);
+
+document.getElementById("intro").style.display = "none";
+
 const toggleModal1 = ((e) => {
     const modal = document.getElementById("modalContainer--medicinaInterna");
     if (modal.style.display === "none") {
